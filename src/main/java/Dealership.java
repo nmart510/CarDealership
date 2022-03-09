9466812493d46b5e5d9cda27d6a4a4f5b82fa1ff
nmart510 <nicholas.pa.ma@gmail.com>
2022-03-09 13:03:01 -0800

    Create Dealership.java

---
+++ src/main/java/Dealership.java
@@ -0,0 +1,53 @@
+package main.java;
+
+import main.java.Models.Vehicle;
+
+public class Dealership {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	boolean login(String fname, String lname, String pass){
+		
+		
+		return false;
+	}
+	public Vehicle[] viewCars() {
+		
+		
+		return null;
+	} //Need to overload
+	public boolean sellCar() {
+		
+		return false;
+	}
+	public boolean buyCar() {
+		
+		return false;
+	}
+	public String postRecall() {
+		
+		return null;
+	}
+	public String generateReport() {
+		
+		return null;
+	}
+	public String generateSalesHistory() {
+		//Will need to make a Sales class for this and return an array of Sales.
+		return null;
+	} //Needs overloads
+	public boolean addVehicle() {
+		
+		return false;
+	}
+	public boolean addCustomer() {
+		
+		return false;
+	}
+	public boolean addEmployee() {
+		
+		return false;
+	}
+}
