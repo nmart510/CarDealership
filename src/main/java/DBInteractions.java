eb6c327a33495bcb7a8d93a8f91d78f0ddf734ec
nmart510 <nicholas.pa.ma@gmail.com>
2022-03-11 15:25:42 -0800

    Create DBInteractions.java

---
+++ src/main/java/DBInteractions.java
@@ -0,0 +1,8 @@
+// This is the ONLY CLASS that connects with the database and runs SQL statements
+// Running SQL statement is the SOLE PURPOSE of this class
+// These methods are called by the Dealership class.
+package main.java;
+
+public class DBInteractions {
+
+}
